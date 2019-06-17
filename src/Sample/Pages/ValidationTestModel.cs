f0e73f9facf3a0184eb0dd69b6974a2e92fed17f
Alessandro Ghidini <ghidello@users.noreply.github.com>
2019-06-17 21:14:47 +0200

    sample validation page

---
+++ src/Sample/Pages/ValidationTestModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sample.Pages
+{
+    public class ValidationTestModel
+    {
+        [Required]
+        [StringLength(5, ErrorMessage = "Name too long (5 character limit).")]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(5, ErrorMessage = "Surname too long (5 character limit).")]
+        public string Surname { get; set; }
+
+        [Required]
+        [StringLength(5, ErrorMessage = "Country too long (5 character limit).")]
+        public string Country { get; set; }
+    }
+}
