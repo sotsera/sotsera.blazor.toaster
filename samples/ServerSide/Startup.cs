d0f050dbea4ee6057e751711afde9f1c4796d3af
Brian Holland <bholland@jackrabbittech.com>
2019-12-24 11:10:02 -0500

    Added left. center, right text position for title and message, in clientside and serverside projects

--- samples/ServerSide/Startup.cs
+++ samples/ServerSide/Startup.cs
@@ -23,6 +23,9 @@ namespace ServerSide
             services.AddToaster(config =>
             {
                 config.PositionClass = Defaults.Classes.Position.TopRight;
+                config.ToastTitleClass = Defaults.Classes.ToastTitle + " " + Defaults.Classes.TextPosition.Left;
+                config.ToastMessageClass = Defaults.Classes.ToastMessage + " " + Defaults.Classes.TextPosition.Left;
+
                 config.PreventDuplicates = true;
                 config.NewestOnTop = false;
             });
